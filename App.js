0dbbd87b1c0ffa0818a45b7f9a313c85e5252846
mohfikifachrezy <fiki.9c.27@gmail.com>
2017-07-20 11:36:22 +0700

    gambar

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, Viewgi } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>New Project React!</Text>
-        <Image
-          source={{ uri: 'http://bit.ly/2tsw0o3' }}
-          style={{ height: 140, width: 200 }}
-        />
       </View>
     );
   }
