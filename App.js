e138cf0e0fb79d3ebf3067430c1db75c35d366a5
mohfikifachrezy <fiki.9c.27@gmail.com>
2017-07-20 10:57:31 +0700

    nyobain

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>New Project React!</Text>
+        <Image
+          source={{ uri: 'http://bit.ly/2tsw0o3' }}
+          style={{ height: 140, width: 200 }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
